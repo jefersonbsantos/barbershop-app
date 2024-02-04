02665f6974698e4891a946714a0302db2f7cd733
Jeferson <jeferson_jb@live.com>
2024-02-04 07:00:14 -0300

    feat: booking component

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,6 +1,34 @@
 import Image from "next/image";
 import Header from "../_components/header";
+import { format } from "date-fns";
+import { ptBR } from "date-fns/locale";
+import Search from "./_components/search";
+import BookingItem from "../_components/booking-item";
 
 export default function Home() {
-    return <Header />;
+    return (
+        <>
+            <Header />
+
+            <div className="px-5 pt-5">
+                <h2 className="text-xl font-bold">Olá, Jeferson</h2>
+                <p className="capitalize text-sm">
+                    {format(new Date(), "EEEE',' dd 'de' MMMM", {
+                        locale: ptBR,
+                    })}
+                </p>
+            </div>
+
+            <div className="px-5 mt-6">
+                <Search />
+            </div>
+
+            <div className="px-5 mt-6">
+                <h2 className="text-xs uppercase text-gray-400 font-bold mb-3">
+                    Agendamentos
+                </h2>
+                <BookingItem />
+            </div>
+        </>
+    );
 }
