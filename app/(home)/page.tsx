5c2b82ed4942912f8487a68c341ca5a9966515c5
Jeferson <jeferson_jb@live.com>
2024-02-04 04:34:14 -0300

    feat: header component

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
+import Header from "../_components/header";
 
 export default function Home() {
-    return <h1>Olá, Mundo!</h1>;
+    return <Header />;
 }
--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,5 +1,26 @@
+import Image from "next/image";
+import { Card, CardContent } from "./ui/card";
+import { Button } from "./ui/button";
+import { MenuIcon } from "lucide-react";
+
 const Header = () => {
-    return <div>Header</div>;
+    return (
+        <header>
+            <Card>
+                <CardContent className="px-5 py-8 justify-between items-center flex">
+                    <Image
+                        src="/logo.png"
+                        alt="Logo Barber"
+                        height={22}
+                        width={120}
+                    />
+                    <Button variant="outline" size="icon">
+                        <MenuIcon size={18} />
+                    </Button>
+                </CardContent>
+            </Card>
+        </header>
+    );
 };
 
 export default Header;
